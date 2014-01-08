3e31f07f4e33fef2634fe0f886510846251d29dd
nikesh <Nikesh.Oswal@wolfsonmicro.com>
2014-01-08 14:58:45 +0000

    ASOC: BCM2708: Update pcm hw info flag
    Signed-off-by: nikesh <Nikesh.Oswal@wolfsonmicro.com>

--- sound/soc/bcm/bcm2708-i2s.c
+++ sound/soc/bcm/bcm2708-i2s.c
@@ -816,8 +816,13 @@ static void bcm2708_i2s_setup_gpio(void)
 }
 
 static const struct snd_pcm_hardware bcm2708_pcm_hardware = {
-	.info			= SNDRV_PCM_INFO_INTERLEAVED |
-				  SNDRV_PCM_INFO_JOINT_DUPLEX,
+	.info			= SNDRV_PCM_INFO_MMAP |
+				  SNDRV_PCM_INFO_MMAP_VALID |
+				  SNDRV_PCM_INFO_INTERLEAVED |
+				  SNDRV_PCM_INFO_PAUSE |
+				  SNDRV_PCM_INFO_RESUME |
+				  SNDRV_PCM_INFO_JOINT_DUPLEX |
+				  SNDRV_PCM_INFO_NO_PERIOD_WAKEUP,
 	.formats		= SNDRV_PCM_FMTBIT_S16_LE |
 				  SNDRV_PCM_FMTBIT_S24_LE |
 				  SNDRV_PCM_FMTBIT_S32_LE,
